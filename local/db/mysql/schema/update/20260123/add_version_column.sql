39d7122714cf36ed523696a636613f3f3b7d3b1c
bbyuk <k941026h@naver.com>
2026-01-23 11:08:16 +0900

    db: 실거래가 버전컬럼 추가 문법오류 수정

--- local/db/mysql/schema/update/20260123/add_version_column.sql
+++ local/db/mysql/schema/update/20260123/add_version_column.sql
@@ -1,5 +1,5 @@
 -- 2026.01.22 실거래가 테이블 버전 컬럼 추가
 -- 작성자 : bbyuk
 
-ALTER TABLE real_estate_sell ADD COLUMN version DATE NULL AFTER y_pos COMMENT '버전';
-ALTER TABLE real_estate_lease ADD COLUMN version DATE NULL AFTER y_pos COMMENT '버전';
+ALTER TABLE real_estate_sell ADD COLUMN version DATE NULL COMMENT '버전' AFTER y_pos;
+ALTER TABLE real_estate_lease ADD COLUMN version DATE NULL COMMENT '버전' AFTER y_pos;
